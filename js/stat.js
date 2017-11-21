cf66ebfb0a81bda0b4980d57923f5eb91e142051
k <admin@localhost>
2017-11-20 23:01:04 -0800

    #8 Учебный проект: Холст

--- js/stat.js
+++ js/stat.js
@@ -1,36 +1,93 @@
 'use strict';
 
+window.renderStatistics = function (ctx, names, times) {
+  // Тень
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(
+      window.renderStatistics.CLOUD_BASE_X_COORDINATE + 10,
+      window.renderStatistics.CLOUD_BASE_Y_COORDINATE + 10,
+      420,
+      270);
+  // Квадратное Облако :)
+  ctx.fillStyle = 'white';
+  ctx.fillRect(
+      window.renderStatistics.CLOUD_BASE_X_COORDINATE,
+      window.renderStatistics.CLOUD_BASE_Y_COORDINATE,
+      420,
+      270);
+  // Сообщение о победе
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+  window.fillTextWordWrap(ctx,
+      'Ура вы победили!\nСписок результатов:',
+      window.renderStatistics.CLOUD_BASE_X_COORDINATE + 40,
+      window.renderStatistics.CLOUD_BASE_Y_COORDINATE + 30);
+
+  window._createStatsTables(ctx, times, names);
+};
+// Constants
+window.renderStatistics.HISTOGRAM_HEIGHT = 150;
+window.renderStatistics.HISTOGRAM_WIDTH = 40;
+window.renderStatistics.CLOUD_BASE_X_COORDINATE = 100;
+window.renderStatistics.CLOUD_BASE_Y_COORDINATE = 10;
+window.renderStatistics.INDENT_SPACE = 80;
+
 /**
- * Добавим немного функциональности в canvas :)
+ * Сгенерировать столбцы гистаграмы.
+ *
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {int} times Массив с временем игроков
+ * @param {int} names Массив с их именами
+ * @private
+ */
+window._createStatsTables = function (ctx, times, names) {
+  var step = window.renderStatistics.HISTOGRAM_HEIGHT / window._getMaxPlayerScore(times);
+
+  var initialX = window.renderStatistics.CLOUD_BASE_X_COORDINATE + 70;
+  var initialY = window.renderStatistics.CLOUD_BASE_Y_COORDINATE + 230;
+  for (var j = 0; j < times.length; j++) {
+    var currentCoordinateX = initialX + window.renderStatistics.INDENT_SPACE * j;
+    var currentHeightY = -(times[j] * step);
+    var currentScoreTextCoordinateY = initialY - (-currentHeightY) - 5;
+
+    // Столбцы
+    ctx.fillStyle = (names[j] === 'Вы') ? 'red' : window._getRgbaRandomTransparencyLevel([0, 0, 255]);
+    ctx.fillRect(currentCoordinateX, initialY, window.renderStatistics.HISTOGRAM_WIDTH, currentHeightY);
+    // Очки на столбцах
+    ctx.fillStyle = 'black';
+    ctx.fillText(names[j], currentCoordinateX, initialY + 20);
+    ctx.fillText(parseInt(times[j], 10), currentCoordinateX, currentScoreTextCoordinateY);
+  }
+};
+
+/**
+ * Написать текст методом fillText() canvas'a с автоматической поддержкой
+ * переносов строки через "\n".
+ *
+ * @param {CanvasRenderingContext2D} ctx
  * @param {string} text
  * @param {int} x
  * @param {int} y
  * @param {int} maxWidth
  */
-CanvasRenderingContext2D.prototype.fillTextWordWrap = function (text, x, y, maxWidth) {
+window.fillTextWordWrap = function (ctx, text, x, y, maxWidth) {
   var keywords = text.split('\n');
-  var currentTextSize = parseInt(this.font, 10);
+  var currentTextSize = parseInt(ctx.font, 10);
   var currentYShift = 0;
   for (var i = 0, keywordsLength = keywords.length; i < keywordsLength; i++) {
-    this.fillText(keywords[i], x, y + currentYShift, maxWidth);
+    ctx.fillText(keywords[i], x, y + currentYShift, maxWidth);
     currentYShift += currentTextSize;
   }
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  var CLOUD_BASE_X_COORDINATE = 100;
-  var CLOUD_BASE_Y_COORDINATE = 10;
-  // Shadow
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(CLOUD_BASE_X_COORDINATE + 10, CLOUD_BASE_X_COORDINATE + 10, 420, 270);
-  // Cloud
-  ctx.fillStyle = 'white';
-  ctx.fillRect(CLOUD_BASE_X_COORDINATE, CLOUD_BASE_Y_COORDINATE, 420, 270);
-  // Show Win/Loose message
-  ctx.fillStyle = 'black';
-  ctx.font = '16px PT Mono';
-  ctx.fillTextWordWrap('Ура вы победили!\nСписок результатов:', CLOUD_BASE_X_COORDINATE + 20, CLOUD_BASE_Y_COORDINATE + 50);
-
+/**
+ * Найти максимальное кол-во очков среди всех игроков.
+ *
+ * @param {int} times
+ * @return {number}
+ * @private
+ */
+window._getMaxPlayerScore = function(times) {
   var max = -1;
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
@@ -38,34 +95,17 @@ window.renderStatistics = function (ctx, names, times) {
       max = time;
     }
   }
-
-  var histogramHeight = 150;
-  var histogramWidth = 40;
-
-  var step = histogramHeight / max;
-
-  var initialX = CLOUD_BASE_X_COORDINATE + 40;
-  var initialY = CLOUD_BASE_Y_COORDINATE + 230;
-  var indent = 80;
-
-  for (var j = 0; j < times.length; j++) {
-    var currentCoordinateX = initialX + indent * j;
-    var currentHeightY = -(times[j] * step);
-
-    ctx.fillStyle = (names[j] === 'Вы') ? 'red' : window.getRgbaRandomTransparencyLevel([0, 0, 255]);
-    ctx.fillRect(currentCoordinateX, initialY, histogramWidth, currentHeightY);
-
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[j], currentCoordinateX, initialY + 20);
-    ctx.fillText(parseInt(times[j], 10), currentCoordinateX, currentHeightY + 20); // <-- не работает чего-то c отрицательными координатами
-  }
+  return max;
 };
 
-window.getHumanFriendlyTime = function (milliseconds) {
-  return (milliseconds / 1000).toFixed(2);
-};
-
-window.getRgbaRandomTransparencyLevel = function (rgbColor) {
+/**
+ * Сгенерировать прозрачность в формате rgba() для цвета rgbColor.
+ *
+ * @param {string|array} rgbColor
+ * @return {string}
+ * @private
+ */
+window._getRgbaRandomTransparencyLevel = function (rgbColor) {
   if (rgbColor.constructor.name === 'Array') {
     rgbColor = rgbColor.join(', ');
   }
